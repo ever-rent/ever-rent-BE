044e8be1a69d6868f541ec2acb67a200012ff819
bsh9254 <bsh9254@khu.ac.kr>
2022-10-16 00:19:20 +0900

    Add myinfo logic

--- src/main/java/com/finalproject/everrent_be/global/jwt/TokenProvider.java
+++ src/main/java/com/finalproject/everrent_be/global/jwt/TokenProvider.java
@@ -28,7 +28,7 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 1;            // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 40;            // 40분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
     private final Key key;
